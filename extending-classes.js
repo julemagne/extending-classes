d5684b79a3b860bb61ca1b611f53921c5b5fa5ac
Julie David <julemagne@users.noreply.github.com>
2016-09-27 18:33:16 -0400

    Add days and due properties and sendCheckoutMessage function to dueDate class

--- extending-classes.js
+++ extending-classes.js
@@ -31,29 +31,44 @@ class dueDate extends myDateFormatter {
 
   discDueDate() {
     this.setDate(new Date().getDate() + 14);
+    this.days = 14;
+    this.due = this.getFormattedDate();
     return this;
   }
   
   bookDueDate() {
     this.setDate(new Date().getDate() + 21);
+    this.days = 21;
+    this.due = this.getFormattedDate();
     return this;
   }
   
   customDueDate(days) {
     this.setDate(new Date().getDate() + days)
+    this.days = days;
+    this.due = this.getFormattedDate();
     return this;
+  }
+  
+  sendCheckoutMessage() {
+    let plural = this.days === 1? "day" : "days";
+    var message = "Your rental is due in " + this.days + " " + plural + " on: " + this.due;
+    return message;
   }
 }
 
 var discRental = new dueDate().discDueDate();
 console.log("Disc Due Date is 14 days:")
-console.log(discRental.getFormattedDate());
+console.log(discRental);
+console.log(discRental.sendCheckoutMessage());
 
 var bookRental = new dueDate().bookDueDate();
 console.log("Book Due Date is 21 days:")
-console.log(bookRental.getFormattedDate());
+console.log(bookRental);
+console.log(bookRental.sendCheckoutMessage());
 
 var flashDriveRental = new dueDate().customDueDate(1);
 console.log("Flash Drive Due Date is 1 day:")
-console.log(flashDriveRental.getFormattedDate());
+console.log(flashDriveRental);
+console.log(flashDriveRental.sendCheckoutMessage());
 
