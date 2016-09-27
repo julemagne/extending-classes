310888a757665cebe3fdfefd01e8e4cbe5d3670b
Julie David <julemagne@users.noreply.github.com>
2016-09-27 18:03:30 -0400

    Add DueDate class

--- extending-classes.js
+++ extending-classes.js
@@ -1,8 +1,10 @@
 "use strict";
 
+console.log("A way to format a Date:");
 var now = new Date();
 console.log((now.getMonth() + 1) + '/' + now.getDate() + '/' + now.getFullYear());
 
+console.log("Let's make a class that formats the date to be easily readable by humans:");
 class myDateFormatter extends Date {
   constructor() {
     super();
@@ -16,4 +18,42 @@ class myDateFormatter extends Date {
 
 var myDateNow = new myDateFormatter();
 console.log(myDateNow.getFormattedDate());
+
+console.log("A way to add 7 days to a Date:");
+myDateNow.setDate(now.getDate() + 7);
+console.log(myDateNow.getFormattedDate());
+
+console.log("Let's make a class that adds days to a Date:");
+class dueDate extends myDateFormatter {
+  constructor() {
+    super();
+  }
+
+  discDueDate() {
+    this.setDate(new Date().getDate() + 14);
+    return this;
+  }
+  
+  bookDueDate() {
+    this.setDate(new Date().getDate() + 21);
+    return this;
+  }
+  
+  customDueDate(days) {
+    this.setDate(new Date().getDate() + days)
+    return this;
+  }
+}
+
+var discRental = new dueDate().discDueDate();
+console.log("Disc Due Date is 14 days:")
+console.log(discRental.getFormattedDate());
+
+var bookRental = new dueDate().bookDueDate();
+console.log("Book Due Date is 21 days:")
+console.log(bookRental.getFormattedDate());
+
+var flashDriveRental = new dueDate().customDueDate(1);
+console.log("Flash Drive Due Date is 1 day:")
+console.log(flashDriveRental.getFormattedDate());
 
